b32ae97c5a149fdf8f4a3493217ca49ec4abbfe3
lyfTech <lyfai521@163.com>
2016-09-08 18:34:01 +0800

    activemq

--- config/src/main/java/com/koala/utils/config/MyBatisConfigurationSingle.java
+++ config/src/main/java/com/koala/utils/config/MyBatisConfigurationSingle.java
@@ -63,7 +63,7 @@ public class MyBatisConfigurationSingle implements EnvironmentAware {
 
     @Bean
     public PageHelper pageHelper(){
-        logger.info("注册MyBatis分页插件PageHelper");
+        logger.info("MyBatis分页插件PageHelper");
         PageHelper pageHelper = new PageHelper();
         Properties p = new Properties();
         p.setProperty("offsetAsPageNum", "true");
